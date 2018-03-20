afd63699d41a8b57053e9d4cd9a969b9e6ccb942
momoh142010 <momoh142010@users.noreply.github.com>
2018-03-21 00:00:45 +0100

    Add files via upload

--- DASI/TP-Backend/src/main/java/fr/insalyon/dasi/tp/dasi/util/AstroTest.java
+++ DASI/TP-Backend/src/main/java/fr/insalyon/dasi/tp/dasi/util/AstroTest.java
@@ -45,7 +45,7 @@ import org.apache.http.message.BasicNameValuePair;
  */
 public class AstroTest {
 
-    final static String MA_CLÉ_ASTRO_API = "XXXXXXXX-Moodle-Clé";
+    final static String MA_CLÉ_ASTRO_API = "ASTRO-02-M0lGLURBU0ktQVNUUk8tQjAy";
 
     public static final String ENCODING_UTF8 = "UTF-8";
     public static final SimpleDateFormat JSON_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
@@ -62,6 +62,11 @@ public class AstroTest {
     public AstroTest(String apiKey) {
         this.apiKey = apiKey;
     }
+    
+    public AstroTest() {
+        this.apiKey = MA_CLÉ_ASTRO_API;
+    }
+    
 
     /*
      * Exemple de Méthode pour appeler le Service Web Profil
